019e3ff7ae550d50e4ec0e561297794cea6a010c
zackthomas1 <zackthomas13@gmail.com>
2025-04-11 14:43:28 -0700

    WIP setting up basic react app with react router.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home'
+import About from './components/About'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // BrowserRouter Wraps entire app to keep UI in sync with URL
+    <BrowserRouter>
+      <div className='App'>
+        {/* Navigation Menu */}
+        <header className='App-header'>
+          <nav className='App-nav'>
+            <ul>
+              {/* Link used to create naviation links without reloading page */}
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </nav>
+          <h1>Local Legend</h1>
+        </header>
+
+        {/* Define Routes */}
+        {/* Routes(v6) replaces older Switch component */}
+        <Routes>
+          {/* Each Route element defines mapping bewteen URL path and corresponding components */}
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
