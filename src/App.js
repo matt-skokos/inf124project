c05c7afb852b0b558acb59343c2018e95f001cb5
zackthomas1 <zackthomas13@gmail.com>
2025-04-15 11:49:57 -0700

    Refactor to use index.css for global style overrides.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home'
-import './App.css'; // global styles
 
 function App() {
   return (
