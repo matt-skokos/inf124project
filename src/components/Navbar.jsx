1b3cf870adc87f40a2fa0fc360aed703b667003f
zackthomas1 <zackthomas13@gmail.com>
2025-04-29 11:25:57 -0700

    Update navbar component to match Figma design.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,60 @@
 import React from 'react'; 
 import { Link } from 'react-router-dom'; 
-
+import logo from '../assets/logo.png'; 
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icons
+import './Navbar.css'; 
 
 function Navbar(){
     return (
-        <nav className='navbar navbar-expand-lg navbar-light bg-light border-bottom'>
-            <div className='container'>
+        <nav className='navbar navbar-expand-lg'>
+            <div className='container flex-column'>
                 
-                {/* Logo */}
-                <Link className='navbar-brand fw-bold' to='/'>
-                    {/*TODO: replace with site logo */}
-                    Local Legend
-                </Link>
+                {/* First row: links + toggler + profile */}
+                <div className='d-flex w-100 justify-content-center align-items-center py-2'>
+                    
+                    {/* Toggle for mobile view */}
+                    <button
+                        className='navbar-toggler'
+                        type='button'
+                        data-bs-toggle='collapse'
+                        data-bs-target='#navbarNav'
+                        aria-controls="navbarNav"
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'
+                    >
+                        <span className='navbar-toggler-icon'></span>
+                    </button>
 
-                {/* Toggle for mobile view */}
-                <button
-                    className='navbar-toggler'
-                    type='button'
-                    data-bs-toggle='collapse'
-                    data-bs-target='#navbarNav'
-                    aria-controls="navbarNav"
-                    aria-expanded='false'
-                    aria-label='Toggle navigation'
-                >
-                    <span className='navbar-toggler-icon'></span>
-                </button>
+                    {/* nav-links (collapse on small screens) */}
+                    <div className='collapse navbar-collapse justify-content-center' id='navbarNav'>
+                        <ul className='navbar-nav'>
+                            <li className='nav-item'><Link className='nav-link' to='/'>Home</Link></li>
+                            <li className='nav-item'><Link className='nav-link' to='/forecast'>Forecast</Link></li>
+                            <li className='nav-item'><Link className='nav-link' to='/explore'>Explore</Link></li>
+                            <li className='nav-item'><Link className='nav-link' to='/favorites'>Favorites</Link></li>
+                        </ul>
+                    </div>
 
-                <div className='collapse navbar-collapse' id='navbarNav'>
-                    <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-                        <li className='nav-item'><Link className='nav-link' to='/'>Home</Link></li>
-                        <li className='nav-item'><Link className='nav-link' to='/forecast'>Forecast</Link></li>
-                        <li className='nav-item'><Link className='nav-link' to='/explore'>Explore</Link></li>
-                        <li className='nav-item'><Link className='nav-link' to='/favorites'>Favorites</Link></li>
-                    </ul>
+                    {/* profile icon always visible */}
+                    <Link to="/login" className='navbar-profile '>
+                        <i class="bi bi-person-circle"></i>
+                    </Link>
                 </div>
 
-                <span className='navbar-profile-icon'>
-                    {/* TODO: replace with profile icon */}
-                    <a href='/login'>profile</a>
-                </span>
+                {/* Second row: Logo + underline */}
+                <div className='w-100 text-center logo-row'>
+                    {/* Logo */}
+                    <Link to="/">
+                        <img 
+                            src={logo}
+                            className="navbar-logo"
+                            alt="Local Legend Logo"
+                        />
+                    </Link>
 
-
+                    {/* Underline */}
+                    <div className='logo-underline'></div>
+                </div>
             </div>
         </nav>
     );
