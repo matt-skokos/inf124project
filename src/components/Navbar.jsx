36c4094c544272ebe658c000791b9fe0ee238da2
zackthomas1 <zackthomas13@gmail.com>
2025-05-01 11:23:17 -0700

    closes #14. Navbar links collapse into menu on small format devices.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,63 +4,112 @@ import logo from '../assets/logo.png';
 import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icons
 import './Navbar.css'; 
 
+
+function MobileNavbar(props)
+{
+    return(
+        <div className='mobile-nav d-lg-none'>
+            <div className={`d-flex ${props.hideTopRow ? 'justify-content-center' : 'justify-content-between'} align-items-center w-100 py-2`}>
+                { !props.hideTopRow && (
+                <button
+                    className='navbar-toggler'
+                    type='button'
+                    data-bs-toggle='collapse'
+                    data-bs-target='#mobileNavbarNav'
+                    aria-controls="mobileNavbarNav"
+                    aria-expanded='false'
+                    aria-label='Toggle navigation'
+                >
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                )}
+
+                {/* Logo */}
+                <Link className='' to="/">
+                    <img 
+                        src={logo}
+                        className="navbar-logo"
+                        alt="Local Legend"
+                    />
+                </Link>
+
+                {/* profile icon */}
+                { !props.hideTopRow && (
+                <Link to="/login" className='navbar-profile'>
+                    <i class="bi bi-person-circle"></i>
+                </Link>
+                )}
+            </div>
+
+            { !props.hideTopRow && (
+            <div className="navbar-collapse justify-content-center alin-items-center w-100 py-2" id="mobileNavbarNav">
+                <ul className='navbar-nav'>
+                    <li className='nav-item'><Link className='nav-link' to='/'>Home</Link></li>
+                    <li className='nav-item'><Link className='nav-link' to='/forecast'>Forecast</Link></li>
+                    <li className='nav-item'><Link className='nav-link' to='/explore'>Explore</Link></li>
+                    <li className='nav-item'><Link className='nav-link' to='/favorites'>Favorites</Link></li>
+                </ul>
+            </div>
+            )}
+        </div>
+
+
+    );
+}
+
+function DesktopNavbar()
+{
+    return(
+        <div className='desktop-nav'>
+            <div className="d-none d-lg-flex justify-content-center alin-items-center w-100 py-2">
+                <ul className='navbar-nav'>
+                    <li className='nav-item'><Link className='nav-link' to='/'>Home</Link></li>
+                    <li className='nav-item'><Link className='nav-link' to='/forecast'>Forecast</Link></li>
+                    <li className='nav-item'><Link className='nav-link' to='/explore'>Explore</Link></li>
+                    <li className='nav-item'><Link className='nav-link' to='/favorites'>Favorites</Link></li>
+                </ul>
+
+                {/* profile icon */}
+                <Link to="/login" className='navbar-profile'>
+                    <i class="bi bi-person-circle"></i>
+                </Link> 
+            </div>
+
+            {/* ---Logo Row---- */}
+            <div className='logo-row d-none d-lg-block w-100 text-center'>
+                {/* Logo */}
+                <Link to="/">
+                    <img 
+                        src={logo}
+                        className="navbar-logo"
+                        alt="Local Legend Logo"
+                    />
+                </Link>
+            </div>
+        </div>
+    );
+}
+
 function Navbar(){
 
-    const location = useLocation(); // Get the current location
-    const hideTopRow = ['/login', '/register'].includes(location.pathname); // Check if the current path is login or registration
+    const {pathname} = useLocation(); // Get the current location
+    const hideTopRow = ['/login', '/register'].includes(pathname); // Check if the current path is login or registration
     
     return (
         <nav className='navbar navbar-expand-lg'>
             <div className='container flex-column'>
-                
-                {/* First row: links + toggler + profile */}
+
+                <MobileNavbar
+                    hideTopRow = {hideTopRow}
+                />
+
                 { !hideTopRow && (
-                <div className='d-flex w-100 justify-content-center align-items-center py-2'>
-                    
-                    {/* Toggle for mobile view */}
-                    <button
-                        className='navbar-toggler'
-                        type='button'
-                        data-bs-toggle='collapse'
-                        data-bs-target='#navbarNav'
-                        aria-controls="navbarNav"
-                        aria-expanded='false'
-                        aria-label='Toggle navigation'
-                    >
-                        <span className='navbar-toggler-icon'></span>
-                    </button>
-
-                    {/* nav-links (collapse on small screens) */}
-                    <div className='collapse navbar-collapse justify-content-center' id='navbarNav'>
-                        <ul className='navbar-nav'>
-                            <li className='nav-item'><Link className='nav-link' to='/'>Home</Link></li>
-                            <li className='nav-item'><Link className='nav-link' to='/forecast'>Forecast</Link></li>
-                            <li className='nav-item'><Link className='nav-link' to='/explore'>Explore</Link></li>
-                            <li className='nav-item'><Link className='nav-link' to='/favorites'>Favorites</Link></li>
-                        </ul>
-                    </div>
-
-                    {/* profile icon always visible */}
-                    <Link to="/login" className='navbar-profile '>
-                        <i class="bi bi-person-circle"></i>
-                    </Link>
-                </div>
+                    <DesktopNavbar/>
                 )}
 
-                {/* Second row: Logo + underline */}
-                <div className='w-100 text-center logo-row'>
-                    {/* Logo */}
-                    <Link to="/">
-                        <img 
-                            src={logo}
-                            className="navbar-logo"
-                            alt="Local Legend Logo"
-                        />
-                    </Link>
+                {/* Underline */}
+                <div className='logo-underline'></div>
 
-                    {/* Underline */}
-                    <div className='logo-underline'></div>
-                </div>
             </div>
         </nav>
     );
