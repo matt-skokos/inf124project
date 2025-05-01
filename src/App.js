a7ca43411ac409ee86ed2f7a41ec30e6707c6c1f
Matt <58489693+matt-skokos@users.noreply.github.com>
2025-05-01 08:30:20 -0700

    explore page created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Explore from "./components/explore/explore.jsx";
 
 function App() {
   return (
     // BrowserRouter Wraps entire app to keep UI in sync with URL
     <BrowserRouter>
-      <div className='app-container'>
-        <Navbar/>
+      <div className="app-container">
+        <Navbar />
 
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
           {/* Add other routes here: Forecast, Explore Spots, Favorites */}
         </Routes>
       </div>
---
+++ src/components/explore/explore.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import Navbar from "../navbar/navbar";
+import "./explore.css";
+import Navbar from "../Navbar";
+
+const Explore = () => {
+  return (
+    <div className="explore-page">
+      <header>Explore</header>
+      <main>
+        {/* Add your explore content here */}
+        <p>Welcome to the Explore page!</p>
+      </main>
+    </div>
+  );
+};
+
+export default Explore;
