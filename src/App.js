972046d2499a96f2e82a6dcfc1154ecde2a216df
Matt <58489693+matt-skokos@users.noreply.github.com>
2025-05-01 08:19:36 -0700

    changed login/register to now show

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home'; 
-import Login from './components/Login';
-import Register from './components/Register';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import HeroLogo from "./components/HeroLogo";
 
 function App() {
   return (
     // BrowserRouter Wraps entire app to keep UI in sync with URL
     <BrowserRouter>
-      <div className='app-container'>
-        <Navbar/>
-
+      <div className="app-container">
+        {window.location.pathname === "/login" ||
+        window.location.pathname === "/register" ? (
+          <HeroLogo />
+        ) : (
+          <Navbar />
+        )}
+        }
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           {/* Add other routes here: Forecast, Explore Spots, Favorites */}
         </Routes>
       </div>
