789dc5a21481be4bc7d6390fc3c3082aa2e66cc4
Matt <58489693+matt-skokos@users.noreply.github.com>
2025-05-06 11:55:28 -0700

    explore interface complete

--- src/components/explore/explore.jsx
+++ src/components/explore/explore.jsx
@@ -1,61 +1,171 @@
 import React, { useEffect, useState } from "react";
-// import Navbar from "../navbar/navbar";
 import "./explore.css";
+import HeartSpot from "./heartComponent.jsx";
+import ContentCard from "../Custom/ContentCard.jsx";
 
 const spots = [
   {
     imgURL: "https://source.unsplash.com/random/150x150?surf",
     title: "Sunset Cliffs",
     description:
-      "A scenic coastal location with stunning sunset views and rugged cliffs. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec risus quam. Sed id nunc nec lorem malesuada luctus. Aenean bibendum nulla sed finibus egestas. ",
+      "A scenic coastal location with stunning sunset views and rugged cliffs. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec risus quam. Sed id nunc nec lorem malesuada luctus. Aenean bibendum nulla sed finibus egestas.",
     skillLevel: "Beginner",
   },
   {
     imgURL: `https://source.unsplash.com/150x150/?surf&sig=${Math.random()}`,
     title: "Black's Beach",
     description:
-      "A peaceful hiking trail leads down to a natural reserve sand break. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec risus quam. Sed id nunc nec lorem malesuada luctus. Aenean bibendum nulla sed finibus egestas. ",
+      "A peaceful hiking trail leads down to a natural reserve sand break. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec risus quam. Sed id nunc nec lorem malesuada luctus. Aenean bibendum nulla sed finibus egestas.",
     skillLevel: "Intermediate",
   },
   {
     imgURL: "https://source.unsplash.com/150x150/?beach,cliff,coast",
     title: "Tourmaline Street",
     description:
-      "An easy access beach break and point break with parking and showers.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec risus quam. Sed id nunc nec lorem malesuada luctus. Aenean bibendum nulla sed finibus egestas. ",
+      "An easy access beach break and point break with parking and showers. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec risus quam. Sed id nunc nec lorem malesuada luctus. Aenean bibendum nulla sed finibus egestas.",
     skillLevel: "Beginner",
   },
 ];
 
-function onToggleSpot() {}
+function ConditionOverview({ children, icon, label }) {
+  return (
+    <div className="condition-item text-center">
+      <i className={`condition-icon ${icon}`}></i>
+      <strong className="condition-label card-subtitle mb-1">{label}</strong>
+      <div className="condition-overview">{children}</div>
+    </div>
+  );
+}
+
+function ConditionDetails({ children, label, identifier }) {
+  return (
+    <li>
+      <strong className="condition-label">{label}</strong>
+      <div className="condition-description px-3" id={identifier}>
+        {children}
+      </div>
+    </li>
+  );
+}
+
+function ConditionCard(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <ContentCard className={`condition-card ${isExpanded ? "expanded" : ""}`}>
+      {isExpanded ? (
+        <div className="condition-details">
+          <p>{props.overview}</p>
+
+          <ul className="list-unstyled">
+            {/* SWELL */}
+            <ConditionDetails label="Swell" identifier="swell-details">
+              <p>{props.swell_details}</p>
+            </ConditionDetails>
+
+            {/* WIND */}
+            <ConditionDetails label="Wind" identifier="wind-details">
+              <p>{props.wind_details}</p>
+            </ConditionDetails>
+
+            {/* Tides */}
+            <ConditionDetails label="Tides" identifier="tides-details">
+              <p> {props.tide_details} </p>
+            </ConditionDetails>
+          </ul>
+        </div>
+      ) : (
+        <div className="condition-summary d-flex justify-content-around w-100">
+          {/* SWELL */}
+          <ConditionOverview icon={"bi bi-tsunami"} label="Swell">
+            <p>
+              {props.swell_direction}
+              <br />
+              {props.swell}
+            </p>
+          </ConditionOverview>
+
+          {/* WIND */}
+          <ConditionOverview icon={"bi bi-wind"} label="Wind">
+            <p>
+              {props.wind_direction}
+              <br />
+              {props.wind}
+            </p>
+          </ConditionOverview>
+
+          {/* TIDE */}
+          <ConditionOverview
+            icon={"bi bi-water"}
+            label="Tide"
+            value={props.tide}
+          >
+            <p>{props.tide}</p>
+          </ConditionOverview>
+        </div>
+      )}
+
+      {/* EXPAND BUTTON */}
+      <div
+        className="expand-icon"
+        onClick={() => setIsExpanded((e) => !e)}
+        aria-label={isExpanded ? "Collapse details" : "Expand details"}
+      >
+        {isExpanded ? (
+          <i className="bi bi-arrows-angle-contract"></i>
+        ) : (
+          <i className="bi bi-arrows-angle-expand"></i>
+        )}
+      </div>
+    </ContentCard>
+  );
+}
 
 const Explore = () => {
-  // useEffect to fetch API data for spots
+  const [selectedSpot, setSelectedSpot] = useState(null); // State for selected spot
+  const [spotList, setSpotList] = useState(spots); // State for spot list
+
+  // useEffect to update images with random URLs
   useEffect(() => {
-    // fetch images
     const randomBeachImages = [
       `https://picsum.photos/seed/${Math.floor(Math.random() * 1000)}/150`,
       `https://picsum.photos/seed/${Math.floor(Math.random() * 1000)}/150`,
       `https://picsum.photos/seed/${Math.floor(Math.random() * 1000)}/150`,
     ];
-    // update json
-    const updatedSpot = spots.map((spot, i) => ({
+
+    const updatedSpotList = spots.map((spot, i) => ({
       ...spot,
       imgURL: randomBeachImages[i],
     }));
 
-    onFetchSpots(updatedSpot);
+    setSpotList(updatedSpotList);
   }, []);
 
-  const [spotList, onFetchSpots] = useState(spots);
+  if (selectedSpot) {
+    return (
+      <div className="container">
+        <div>
+          <button onClick={() => setSelectedSpot(null)}>← Back to Spots</button>
+        </div>
+        <div className="explore-page">
+          <SpotDetail spot={selectedSpot} />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
       <div className="explore-page">
         <h1>Explore Spots</h1>
-        {/* {Explore Spots items here} */}
-        <ul>
+        <ul className="spotItems">
           {spotList.map((spot) => (
-            <li className="content-card" key={spot.title}>
-              <Spot {...spot} />
+            <li
+              className="content-card"
+              key={spot.title}
+              onClick={() => setSelectedSpot(spot)}
+            >
+              <Spot {...spot} onClick={() => setSelectedSpot(spot)} />
             </li>
           ))}
         </ul>
@@ -66,8 +176,8 @@ const Explore = () => {
 
 export default Explore;
 
-const Spot = ({ imgURL, title, description, skillLevel }) => {
-  return (
+const Spot = ({ imgURL, title, description, skillLevel, onClick }) => (
+  <div className="spot-card" onClick={onClick}>
     <div
       style={{
         display: "flex",
@@ -85,6 +195,109 @@ const Spot = ({ imgURL, title, description, skillLevel }) => {
           <strong>Skill Level: </strong>
           {skillLevel}
         </h3>
+      </div>
+    </div>
+  </div>
+);
+
+const SpotDetail = ({ spot }) => {
+  // Get today's date in a readable format
+  const today = new Date().toLocaleDateString(undefined, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  // Props for the spot detail
+  const idealTide = spot.idealTide || "High tide";
+  const crowdFactor = spot.crowdFactor || "Generally uncrowded";
+
+  return (
+    <div className="container ">
+      <div className="spot-detail-centered">
+        <div className="spot-detail">
+          <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+            <HeartSpot color="#23718f" size={32} className="heart-button" />
+            {/* Social Media Share Button (placeholder) */}
+            <button
+              type="button"
+              className="btn btn-outline-secondary social-media-button"
+              style={{
+                borderRadius: "50%",
+                width: 32,
+                height: 32,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 0,
+                margin: 0,
+              }}
+              title="Share"
+              aria-label="Share"
+              tabIndex={0}
+            >
+              <i
+                className="bi bi-share-fill"
+                style={{ fontSize: "1.5rem" }}
+              ></i>
+            </button>
+            <h1 className="detail-name">{spot.title}</h1>
+          </div>
+          <div style={{ display: "flex", gap: "1rem", margin: "1rem 0" }}>
+            {[...Array(3)].map((_, index) => (
+              <img
+                key={index}
+                src={spot.imgURL}
+                alt={spot.title}
+                style={{
+                  width: 200,
+                  height: 200,
+                  objectFit: "cover",
+                  padding: ".5rem",
+                }}
+              />
+            ))}
+          </div>
+          {/* Date between images and details */}
+          <div className="spot-date-row">{today}</div>
+          <div style={{ width: "100%" }}>
+            <ConditionCard
+              className=""
+              overview="Surfers can expect moderate wave activity with favorable wind and tide conditions in the afternoon. However, the water temperature is quite cool, so appropriate wetsuits are recommended."
+              swell_direction="SSW"
+              swell="2-3 ft"
+              swell_details="Approximately 3 feet with a primary south-southwest (SSW) swell of 4.5 feet at 18-second intervals"
+              wind_direction="SSW"
+              wind="4 mph"
+              wind_details="Light and variable winds, around 4 mph from the southwest."
+              tide="Low"
+              tide_details="Low tide at 3:26 PM: 0.66 feet. High tide at 9:40 PM: 5.16 feet"
+            />
+          </div>
+          <div style={{ width: "100%", marginTop: "1rem" }}>
+            <h2>Spot Overview</h2>
+            {/* New ContentCard for attributes */}
+            <ContentCard className="spot-attributes-card">
+              <div
+                className="d-flex flex-row justify-content-between w-100"
+                style={{ gap: "2rem" }}
+              >
+                <div>
+                  <strong>Skill Level</strong>
+                  <div>{spot.skillLevel}</div>
+                </div>
+                <div>
+                  <strong>Ideal Tide</strong>
+                  <div>{idealTide}</div>
+                </div>
+                <div>
+                  <strong>Crowd Factor</strong>
+                  <div>{crowdFactor}</div>
+                </div>
+              </div>
+            </ContentCard>
+          </div>
+        </div>
       </div>
     </div>
   );
