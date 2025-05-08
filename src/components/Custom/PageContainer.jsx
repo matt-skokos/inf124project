b6b140c936204d141860ad9135ac04b9e3b0b106
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2025-05-08 15:23:07 -0700

    Pass className to as prop into PageContainer component.

--- src/components/Custom/PageContainer.jsx
+++ src/components/Custom/PageContainer.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import "./PageContainer.css";
 
-function PageContainer({children}){
+function PageContainer({children, className}){
     return(
-        <div className="page-container p-2">
+        <div className={className + "page-container p-2"}>
             {children}
         </div>
     );
