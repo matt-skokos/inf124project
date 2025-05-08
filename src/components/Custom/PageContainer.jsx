e8c15e6c17c20ecdeeaa8bca0c3eddbc3607f13c
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2025-05-08 16:32:24 -0700

    Add <h1> element for page title to PageContainer component.

--- src/components/Custom/PageContainer.jsx
+++ src/components/Custom/PageContainer.jsx
@@ -1,9 +1,14 @@
 import React from "react"
 import "./PageContainer.css";
 
-function PageContainer({children, className=""}){
+function PageContainer({children, className="", title, hideTitle = false}){
     return(
         <div className={`${className} page-container p-2`}>
+            {title && (
+                <h1 className={`${hideTitle ? "visually-hidden" : ""} page-title`}>
+                    {title}
+                </h1>
+            )}
             {children}
         </div>
     );
