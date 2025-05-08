0175db5510930c4a7842be1468d85e5a5ea6de97
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2025-05-08 15:31:28 -0700

    Modify PageContainer component to pass className to div element.

--- src/components/Custom/PageContainer.jsx
+++ src/components/Custom/PageContainer.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import "./PageContainer.css";
 
-function PageContainer({children, className}){
+function PageContainer({children, className=""}){
     return(
-        <div className={className + "page-container p-2"}>
+        <div className={`${className} page-container p-2`}>
             {children}
         </div>
     );
