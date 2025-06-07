3681967f7a4810ecad6ac1ba8e3e5fb4605ebcc2
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2025-06-06 23:37:39 -0700

    Minor style refactor

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,7 +1,13 @@
 const metersToFeet = (meters) => meters * 3.28084;
 
 const degreesToDirection = (degree) => {
-    const directions = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]; 
+    const directions = [
+        "N", "NNE", "NE", "ENE",
+        "E", "ESE", "SE", "SSE",
+        "S", "SSW", "SW", "WSW",
+        "W", "WNW", "NW", "NNW"
+    ];
+
     degrees = degree % 360
     index = Math.round(degree / 22.5) % 16
     return directions[index];
